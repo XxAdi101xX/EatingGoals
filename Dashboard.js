8dad39010e1557d3413035877a0071b742739796
Adithya V <adi_101@live.com>
2017-01-29 12:01:32 -0500

    nothing

--- Dashboard.js
+++ Dashboard.js
@@ -9,4 +9,26 @@ $(document).ready(function() {
     if (localStorage.getItem("recentlyUpdated") == true) {
         alert("hiiiii");
     }
+    var ctx = $("#myChart");
+    var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Item 1', 'Item 2', 'Item 3'],
+        datasets: [
+            {
+                type: 'bar',
+                label: 'Bar Component',
+                data: [10, 20, 30],
+            },
+            {
+                type: 'line',
+                label: 'Line Component',
+                data: [30, 20, 10],
+            }
+        ]
+    },
+    options: {
+        responsive: false
+    }
+});
 });
