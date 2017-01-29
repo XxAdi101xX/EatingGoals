34b08039c7a51b3e3ed2bebd489ce8c2a88777cd
Eric Lin <spaceric@Erics-MacBook-Pro.local>
2017-01-28 19:41:27 -0500

    Initial Calorie API Call Impletmentation

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
     hi();
+    getCalories("celery");
 });
 
 function hi() {
     alert ('hello yall');
+}
+
+//new
+function getCalories(foodItem) {
+    var rawData = new XMLHttpRequest();
+    rawData.open("GET", "https://api.nutritionix.com/v1_1/search/" + foodItem + "?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat&appId=153575d3&appKey=bbc19981ed92ff49f85780068c396b71", false);
+    rawData.send();
+    //alert(data.responseText);
+    var data = JSON.parse(rawData.responseText);
+    //alert(data);
 }
 
 //alert('hi');
