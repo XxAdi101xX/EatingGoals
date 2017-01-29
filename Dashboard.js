ab8ef87623ac107ca886db581bcf5355d80889ea
Adithya V <adi_101@live.com>
2017-01-29 06:54:56 -0500

    buttons all work now

---
+++ Dashboard.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("#addMore").click(function() {
+        window.location = "index.html";
+    });
+
+     $(".link").click(function() {
+        window.location = "profile.html";
+    });
+});
